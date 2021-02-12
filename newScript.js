d6a5ddb82b822c4e3fa48cb40e42923293522eba
isabella.mattua <isa_mattua@hotmail.com>
2021-02-12 17:38:31 -0500

    API Done

--- newScript.js
+++ newScript.js
@@ -1,14 +1,19 @@
 // Grab text and button elements from HTML document
 
 //YES or NO API
-var pickUpLine = document.querySelector("#YESorNO");
+var ynString = document.querySelector("#YESorNO");
 var btnYN = document.querySelector("#YNbtn");
+
 
 
 //Joke API
 var jokePunchline = document.querySelector("#jokePunchline")
 var jokeSetup = document.querySelector("#jokeSetup")
 var btnJOKE = document.querySelector("#jokeButton");
+
+//PickupLine API (OURS)
+var pickUpLine = document.querySelector("#pickUpLine");
+var btnPICKUPLINE = document.querySelector("#pickuplineButton");
 
 // Asynchronous function that gets data from external API
 async function generateYesNO() {
@@ -38,6 +43,17 @@ async function generateJoke() {
 
 }
 
+
+async function generatePickupLine() {
+    var response = await fetch("http://127.0.0.1:5000/randomQuote");
+    var data = await response.json();
+    line = data.saying;
+    console.log(line);
+    document.querySelector("#pickUpLine").innerHTML = line;
+}
+
 // Add event listener to each button when clicked
 btnYN.addEventListener('click', generateYesNO);
 btnJOKE.addEventListener('click', generateJoke);
+btnPICKUPLINE.addEventListener('click', generatePickupLine)
+
